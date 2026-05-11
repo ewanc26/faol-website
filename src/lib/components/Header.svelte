00d3188e2b3f4eeafcbe983a09cab9f6d46cbac2
Ewan Croft <git@ewancroft.uk>
2026-05-11 14:43:37 +0100

    fix: redesign wolf icon with bezier curves (Lucide family)
    - Single closed cubic-bezier path for head silhouette (like lucide-cat)
    - No more polygon L-commands on the outline
    - Eyes as horizontal H strokes
    - Nose as tight chevron
    - Removed separate inner-ear strokes (silhouette handles ears)

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -13,13 +13,11 @@
 <header class="site-header">
 	<a href="/" class="site-name" aria-label="Home">
 		<svg class="wolf-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-			<path d="M5 2L2 10L4 14L7 18L10 20L12 21L14 20L17 18L20 14L22 10L19 2" />
-			<path d="M5 5L4 10" />
-			<path d="M19 5L20 10" />
-			<path d="M8.5 12.5H10.5" />
-			<path d="M13.5 12.5H15.5" />
-			<path d="M10.5 16L12 17.5L13.5 16" />
-		</svg>
+		<path d="M12 5c.8 0 1.6.12 2.4.35 1.5-2.2 4.8-3 6.1-2.3 1.3.7-.5 7-.5 7 .6 1.1 1 2.3 1 3.6C21 17.7 17 21 12 21S3 17.7 3 13.65c0-1.3.4-2.5 1-3.6 0 0-1.8-6.3-.5-7 1.3-.7 4.6.1 6.1 2.3A8.8 8.8 0 0 1 12 5Z" />
+		<path d="M8.5 12.5H10" />
+		<path d="M14 12.5H15.5" />
+		<path d="M11 16.5L12 17.5L13 16.5" />
+	</svg>
 		faol
 	</a>
 	<nav>
