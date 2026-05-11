2b185334c3c810c03c1f9a2af3b2c3a108998f6c
Ewan Croft <git@ewancroft.uk>
2026-05-11 14:12:38 +0100

    fix: make content column dynamic at 65% of container

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,7 +23,7 @@
 		<Header current={page.url.pathname} />
 	</div>
 	<main class="flex-1 px-6 py-10 max-w-[80vw] w-full mx-auto">
-		<div class="max-w-[560px]">
+		<div class="max-w-[65%]">
 			{@render children()}
 		</div>
 	</main>
